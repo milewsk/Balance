3cd5f325c8c7891a93fd0187d208e6195aa45308
Konrad Milewski <konradmilewski2000@interia.eu>
2022-05-14 17:52:47 +0200

    Add accountSlice
    Add account slice whitch gonna take care or account actions

--- src/app/components/Navigation/DestopNavigation.tsx
+++ src/app/components/Navigation/DestopNavigation.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { deflate } from "zlib";
 import { useAppDispatch, useAppSelector } from "../../store/storeHooks";
 
 const DesktopNavigation = (): JSX.Element => {
   const typeOfNavigation = useAppSelector((state) => state.navigation);
   const dispatch = useAppDispatch();
-  return <div>desktop nav</div>;
+  return (
+    <nav className="navigation__desktop">
+      <Link className="navigation__logo" to="/home">
+        Balance
+      </Link>
+      <ul>
+        <li>
+          <Link to="/about"></Link>
+        </li>
+        <li>
+          <Link to="/about"></Link>
+        </li>
+        <li>{<button>Sign in</button>}</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default DesktopNavigation;
