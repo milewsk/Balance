953f4b2952ecb03933d6d4c1294ec8ff58ac2b4a
Konrad Milewski <konradmilewski2000@interia.eu>
2022-05-27 00:06:16 +0200

    End Feature
    End of build base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Account from "./app/components/Accounts/Account";
+import AccountHistory from "./app/components/Accounts/History/AccountHistory";
+import History from "./app/components/Accounts/History/History";
 import Home from "./app/components/Home/Home";
 import Layout from "./app/components/Layout/Layout";
 import Login from "./app/components/Login/Login";
+import Operation from "./app/components/Operation/Operation";
 import Register from "./app/components/Register/Register";
 import Welcome from "./app/components/Start/Welcome";
 import "./sass/app.scss";
@@ -23,10 +27,14 @@ function App() {
           <Route path="home" element={<Home></Home>}></Route>
           <Route path="account">
             <Route path="history">
-              <Route path=":accountId"></Route>
+              <Route path=":accountId">
+                <AccountHistory></AccountHistory>
+              </Route>
+              <Route index element={<History></History>}></Route>
             </Route>
+            <Route index element={<Account></Account>}></Route>
           </Route>
-          <Route path="operation"></Route>
+          <Route path="operation" element={<Operation></Operation>}></Route>
           <Route path="createPDF"></Route>
         </Route>
       )}
--- src/app/components/Login/Login.tsx
+++ src/app/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { FormEventHandler, Fragment, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "../../../sass/components/account/login.scss";
 import "../../../sass/components/button/button.scss";
@@ -6,6 +6,60 @@ import "../../../sass/components/button/button.scss";
 import { useAppDispatch, useAppSelector } from "../../store/storeHooks";
 
 const Login = (): JSX.Element => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    fetch("https://localhost:44360/api/accounts", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const SendRequest = async (email: string, passowrd: string) => {
+    try {
+      const response = await fetch(
+        `https://localhost:44360/api/user/${email}/${passowrd}`,
+        { method: "GET" }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        const data = await response.json();
+        throw new Error(data.message);
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const submitHandler = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formElements = form.elements as typeof form.elements & {
+      emailInput: { value: string };
+      passwordInput: { value: string };
+    };
+
+    const email: string = formElements.emailInput.value;
+    const password: string = formElements.passwordInput.value;
+
+    SendRequest(email, password);
+  };
+
   return (
     <div className="login-page">
       <div className="login-page__intro">
@@ -14,12 +68,20 @@ const Login = (): JSX.Element => {
           Balance - kontroluj swoją firmę
         </div>
       </div>
-      <form className="login-page__form">
+      <form className="login-page__form" ref={formRef} onSubmit={submitHandler}>
         <h2 className="login-page__title">Zaloguj się</h2>
         <label>Email</label>
-        <input placeholder="Expample@gmail.com" type="text"></input>
+        <input
+          placeholder="Expample@gmail.com"
+          type="text"
+          id="emailInput"
+        ></input>
         <label>Hasło</label>
-        <input placeholder="Wpisz swoje hasło" type="passowrd"></input>
+        <input
+          placeholder="Wpisz swoje hasło"
+          type="passowrd"
+          id="passwordInput"
+        ></input>
         <p className="login-page__forgot-password">Nie pamiętasz hasła?</p>
         <p className="login-page__redirect">
           Nie masz konta? <Link to="/register">Zarejestruj się</Link>
